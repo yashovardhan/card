b84545e17e91e6181397d8306583fc917b48bdcb
Yashovardhan Agrawal <yashovardhan.agrawal@gmail.com>
2020-09-06 20:22:21 +0530

    Add card

---
+++ README.md
@@ -0,0 +1,11 @@
+# Usage
+
+## npm
+```
+npx yashovardhan
+```
+
+## GitHub Package Registry
+```
+npx @yashovardhan/card
+```
---
+++ build.js
@@ -0,0 +1,54 @@
+'use strict'
+
+// Pull in our modules
+const chalk = require('chalk')
+const boxen = require('boxen')
+const fs = require('fs')
+const path = require('path')
+
+// Define options for Boxen
+const options = {
+  padding: 1,
+  margin: 1,
+  borderStyle: 'round'
+}
+
+// Text + chalk definitions
+const data = {
+  name: chalk.white('             Yashovardhan Agrawal'),
+  handle: chalk.white('yashovardhan'),
+  work: chalk.white('Hackathon Community Manager at Major League Hacking'),
+  twitter: chalk.gray('https://twitter.com/') + chalk.cyan('yashovardhan'),
+  github: chalk.gray('https://github.com/') + chalk.green('yashovardhan'),
+  linkedin: chalk.gray('https://linkedin.com/in/') + chalk.blue('yashovardhanagrawal'),
+  web: chalk.cyan('https://yashovardhan.me'),
+  npx: chalk.red('npx yashovardhan'),
+  labelWork: chalk.white.bold('      Work:'),
+  labelTwitter: chalk.white.bold('   Twitter:'),
+  labelGitHub: chalk.white.bold('    GitHub:'),
+  labelLinkedIn: chalk.white.bold('  LinkedIn:'),
+  labelWeb: chalk.white.bold('       Web:'),
+  labelCard: chalk.white.bold('      Card:')
+}
+
+// Actual strings we're going to output
+const newline = '\n'
+const heading = `${data.name} / ${data.handle}`
+const working = `${data.labelWork}  ${data.work}`
+const twittering = `${data.labelTwitter}  ${data.twitter}`
+const githubing = `${data.labelGitHub}  ${data.github}`
+const linkedining = `${data.labelLinkedIn}  ${data.linkedin}`
+const webing = `${data.labelWeb}  ${data.web}`
+const carding = `${data.labelCard}  ${data.npx}`
+
+// Put all our output together into a single variable so we can use boxen effectively
+const output = heading + // data.name + data.handle
+               newline + newline + // Add one whole blank line
+               working + newline + // data.labelWork + data.work
+               twittering + newline + // data.labelTwitter + data.twitter
+               githubing + newline + // data.labelGitHub + data.github
+               linkedining + newline + // data.labelLinkedIn + data.linkedin
+               webing + newline + newline + // data.labelWeb + data.web
+               carding // data.labelCard + data.npx
+
+fs.writeFileSync(path.join(__dirname, 'bin/output'), chalk.green(boxen(output, options)))
